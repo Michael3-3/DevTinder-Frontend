9073531b03ba7b4ee350c12eb467916d91252618
mikhel <mikhelpottella@gmailcom>
2025-08-17 21:59:48 +0530

    fix: update socket connection to use polling transports for testing

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,11 @@
-import io from 'socket.io-client';
-import { BaseUrl } from './statics';
+import io from "socket.io-client";
+import { BaseUrl } from "./statics";
 
 export const createSocketConnection = () => {
-  return io(BaseUrl, {
-    transports: ['polling'],  // force polling for testing
-  });
+  if (location.hostname === "localhost") {
+    return io(BaseUrl);
+  }
+  else{
+    return io("/",{path:"/api/socket.io"});
+  }
 };
