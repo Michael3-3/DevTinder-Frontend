e77643fa2dc243a09a4517332268e9947c55b2b1
mikhel <mikhelpottella@gmailcom>
2025-08-17 20:05:52 +0530

    fix: add transports option to socket connection for testing

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,7 @@ import io from 'socket.io-client';
 import { BaseUrl } from './statics';
 
 export const createSocketConnection = () => {
-    return io(BaseUrl)
-}
+  return io(BaseUrl, {
+    transports: ['polling'],  // force polling for testing
+  });
+};
